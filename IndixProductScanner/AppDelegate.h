d292959f094198ff99fd881f93ce6e05889f80a1
nalin <nalinchhajer@yahoo.com>
2016-02-11 14:23:24 +0530

    Initial Commit

---
+++ IndixProductScanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IndixProductScanner
+//
+//  Created by Nalin Chhajer on 11/02/16.
+//  Copyright © 2016 Indix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IndixProductScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IndixProductScanner
+//
+//  Created by Nalin Chhajer on 11/02/16.
+//  Copyright © 2016 Indix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
